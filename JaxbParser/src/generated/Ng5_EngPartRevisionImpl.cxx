fd63b093765c5acaa5439f3a7d233e0465b3572d
avinashmandru <40795596+avinashmandru@users.noreply.github.com>
2018-08-13 13:41:07 +0530

    user story #4002: Added new FIle

---
+++ JaxbParser/src/generated/Ng5_EngPartRevisionImpl.cxx
@@ -0,0 +1,62 @@
+/*********************************************************************************************
+** File Name:         Ng5_EngPartRevisionImpl.cxx
+**
+** File Description:
+	This file contains the implementation for the Business Object Ng5_EngPartRevisionImpl
+**
+** History:
+**   mm/dd/yyyy  Name          Comments
+**   ----------  ------------- -------------------------
+**   12/09/2016  Avinash Mandru      Initial Version
+	
+*********************************************************************************************/
+
+
+#include <Ng5Core/Ng5_EngPartRevisionImpl.hxx>
+
+#include <fclasses/tc_string.h>
+#include <Ng5Core/Ng5Core_Std_Defines.h>
+#include <tc/tc.h>
+#include "Ng5_CommonUtils.hxx"
+
+using namespace ng5newgeneration;
+
+
+//----------------------------------------------------------------------------------
+// Ng5_EngPartRevisionImpl::Ng5_EngPartRevisionImpl(Ng5_EngPartRevision& busObj)
+// Constructor for the class
+//----------------------------------------------------------------------------------
+Ng5_EngPartRevisionImpl::Ng5_EngPartRevisionImpl( Ng5_EngPartRevision& busObj )
+   : Ng5_EngPartRevisionGenImpl( busObj )
+{
+}
+
+//----------------------------------------------------------------------------------
+// Ng5_EngPartRevisionImpl::~Ng5_EngPartRevisionImpl()
+// Destructor for the class
+//----------------------------------------------------------------------------------
+Ng5_EngPartRevisionImpl::~Ng5_EngPartRevisionImpl()
+{
+}
+
+//----------------------------------------------------------------------------------
+// Ng5_EngPartRevisionImpl::initializeClass
+// This method is used to initialize this Class
+//----------------------------------------------------------------------------------
+int Ng5_EngPartRevisionImpl::initializeClass()
+{
+    int ifail = ITK_ok;
+    static bool initialized = false;
+
+    if( !initialized )
+    {
+        ifail = Ng5_EngPartRevisionGenImpl::initializeClass( );
+        if ( ifail == ITK_ok )
+        {
+            initialized = true;
+        }
+    }
+    return ifail;
+}
+
+
